e515eae1fdd1224ee64a2e520b8c783b00173d46
frnzska <fadler@babbel.com>
2017-11-30 15:42:00 +0100

    add test

--- bar.lambda/tests/test_dummy.py
+++ bar.lambda/tests/test_dummy.py
@@ -3,3 +3,7 @@ from src.MyFile import some_function
 
 def test_dummy():
     assert True
+
+def test_some_function():
+    assert 'bailando' == some_function()
+    assert 'helloWorld' != some_function()
