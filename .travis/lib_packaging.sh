4c36e8674b4a92381b8fef274e326f9ef0fe6751
frnzska <fadler@babbel.com>
2018-04-06 10:20:21 +0200

    exclude dest and tests folder

--- .travis/lib_packaging.sh
+++ .travis/lib_packaging.sh
@@ -5,7 +5,7 @@ echo "creating dest folder..."
 rm -rf dest && mkdir dest
 
 echo "copying source code..."
-cp -r * dest
+cp -r `ls -A | grep -v "dest\|tests"` dest
 
 echo "install production dependencies..."
 cd dest
